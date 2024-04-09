291d039800c71b628a180d10e2fc5f4aea4ee6bd
gin <73906888+gingkapls@users.noreply.github.com>
2024-04-09 21:36:28 +0530

    renderMenu: use documentFragment to reduce jarring

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import OrderButton from "./components/OrderButton";
 import menu from "./menu";
 
 const content = document.querySelector(".content");
+const mainContainer = document.querySelector(".main-container");
+const navBar = document.querySelector("nav");
 
-const renderMenu = () => {
+const renderMenu = (container) => {
+  const fragment = new DocumentFragment();
+  container.replaceChildren();
   menu.forEach((item) => {
-    content.appendChild(FoodItem(item));
+    fragment.appendChild(FoodItem(item));
   });
+
+  container.appendChild(fragment);
 };
 
-renderMenu();
+navBar.addEventListener("click", (event) => {
+  if (event.target === event.currentTarget) return;
+
+  console.log(event.target.textContent);
+  switch (event.target.textContent.toLowerCase()) {
+    case "menu":
+      renderMenu(content);
+      break;
+  }
+});
